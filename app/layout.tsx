3237b52a674e1dea13bf72cc036f8cdc218e1b53
RuanMingze <xmt20160124@outlook.com>
2026-06-13 14:29:45 +0800

    fix: 移除layout.tsx中残留的SnowfallEffect组件引用

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,6 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <GlobalErrorHandler />
           <NavigationProgress />
-          <SnowfallEffect />
           {children}
           <AosInit />
           <AosControl />
